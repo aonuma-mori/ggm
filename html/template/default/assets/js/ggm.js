766b6da082375cc88efff6e00444889957f944ca
aonuma <aonuma.mori@gmail.com>
2020-08-22 13:11:26 +0900

    トップバナー切り替え

--- html/template/default/assets/js/ggm.js
+++ html/template/default/assets/js/ggm.js
@@ -5,4 +5,69 @@
  * vue.js es6
  */
 
- 
+ // Hello
+var app = new Vue({
+  el: '#app',
+  data: {
+    foobar: 'baz'
+  },
+  delimiters: ['[[', ']]'],
+});
+
+/**
+ * Deviceの幅を取得する
+ * 
+ * /html/template/default/assets/scss/ggm-styles.scss
+ */
+var device = new Vue({
+  el: '#device',
+  delimiters: ['[[', ']]'],
+  data: function () {
+    return {
+      width_type: this.width_type
+    }
+  },
+  methods: {
+    handleResize: function() {
+      // console.log(get_inner_width_type());
+      this.width = window.innerWidth;
+      this.width_type = get_inner_width_type();
+      Vue.prototype.$window_width = this.width_type;    
+    }
+  },
+  mounted: function () {
+    this.handleResize(); //ページ表示時に実行させる
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
+  }
+});
+
+/**
+ * Device widthの取得
+ */
+function get_inner_width() {
+  this.width = window.innerWidth;
+  return this.width;
+}
+
+/**
+ * Device type (bootstrap)の取得
+ */
+function get_inner_width_type() {
+  this.width = window.innerWidth;
+  // console.log(this.width);
+  if (this.width <= 576) {
+    this.width_type = 'xs';
+  } else if (this.width > 576 && this.width <= 991) {
+    this.width_type = 'sm';
+  } else if (this.width > 991 && this.width <= 1199) {
+    this.width_type = 'md';
+  } else if (this.width > 1200) {
+    this.width_type = 'lg';
+  } else {
+    this.width_type = 'def:error';
+  }
+  return this.width_type;
+}
