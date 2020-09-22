ab579a86bb4d6b5f3d931a1a2a2170b24649f16d
aonuma <aonuma.mori@gmail.com>
2020-09-23 00:17:56 +0900

    不要ファイル削除

--- starter 2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose up -d
-docker-compose ps -a
-sleep 5
-docker exec ec-cube_mysql_1 mysqldump -u dbuser -psecret eccubedb < var/tmp/develop_dump.sql
--- utility 2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-php composer.phar update;
-rm -f var/cache/dev/*
-rm -f var/cache/prod/*
