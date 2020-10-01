945ef5b5a0389cc46c8c0593786d7f57f6407a8f
aonuma <aonuma.mori@gmail.com>
2020-10-01 12:00:21 +0900

    買い物ページとメンテナンス画面

--- maintenance.php
+++ maintenance.php
@@ -1,37 +1,100 @@
 <!doctype html>
-<?php /*
-This file is part of EC-CUBE
-
-Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
-
-http://www.ec-cube.co.jp/
-
-For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
-*/ ?>
-<html lang="<?php echo $locale; ?>">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ただいまメンテナンス中です。</title>
-    <link rel="icon" href="<?php echo $baseUrl; ?>/html/template/<?php echo $templateCode; ?>/assets/img/common/favicon.ico">
-    <link rel="stylesheet" href="<?php echo $baseUrl; ?>/html/template/<?php echo $templateCode; ?>/assets/css/style.css">
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# product: http://ogp.me/ns/product#">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="eccube-csrf-token" content="jRU-qvfoWXzZNxjy9jpBHsrAUMCYIJNEMRF6MwBcOyE">
+  <title>メンテナンス中 | GGM</title>
+  <meta name="robots" content="noindex">
+  <meta name="msapplication-square70x70logo" content="/html/template/default/assets/img/favicons/site-tile-70x70.png">
+  <meta name="msapplication-square150x150logo" content="/html/template/default/assets/img/favicons/site-tile-150x150.png">
+  <meta name="msapplication-wide310x150logo" content="/html/template/default/assets/img/favicons/site-tile-310x150.png">
+  <meta name="msapplication-square310x310logo" content="/html/template/default/assets/img/favicons/site-tile-310x310.png">
+  <meta name="msapplication-TileColor" content="#0078d7">
+  <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/html/template/default/assets/img/favicons/favicon.ico">
+  <link rel="icon" type="image/vnd.microsoft.icon" href="/html/template/default/assets/img/favicons/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="/html/template/default/assets/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/html/template/default/assets/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/html/template/default/assets/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/html/template/default/assets/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/html/template/default/assets/img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/html/template/default/assets/img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/html/template/default/assets/img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/html/template/default/assets/img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/html/template/default/assets/img/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="36x36" href="/html/template/default/assets/img/favicons/android-chrome-36x36.png">
+  <link rel="icon" type="image/png" sizes="48x48" href="/html/template/default/assets/img/favicons/android-chrome-48x48.png">
+  <link rel="icon" type="image/png" sizes="72x72" href="/html/template/default/assets/img/favicons/android-chrome-72x72.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/html/template/default/assets/img/favicons/android-chrome-96x96.png">
+  <link rel="icon" type="image/png" sizes="128x128" href="/html/template/default/assets/img/favicons/android-chrome-128x128.png">
+  <link rel="icon" type="image/png" sizes="144x144" href="/html/template/default/assets/img/favicons/android-chrome-144x144.png">
+  <link rel="icon" type="image/png" sizes="152x152" href="/html/template/default/assets/img/favicons/android-chrome-152x152.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="/html/template/default/assets/img/favicons/android-chrome-192x192.png">
+  <link rel="icon" type="image/png" sizes="256x256" href="/html/template/default/assets/img/favicons/android-chrome-256x256.png">
+  <link rel="icon" type="image/png" sizes="384x384" href="/html/template/default/assets/img/favicons/android-chrome-384x384.png">
+  <link rel="icon" type="image/png" sizes="512x512" href="/html/template/default/assets/img/favicons/android-chrome-512x512.png">
+  <link rel="icon" type="image/png" sizes="36x36" href="/html/template/default/assets/img/favicons/icon-36x36.png">
+  <link rel="icon" type="image/png" sizes="48x48" href="/html/template/default/assets/img/favicons/icon-48x48.png">
+  <link rel="icon" type="image/png" sizes="72x72" href="/html/template/default/assets/img/favicons/icon-72x72.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/html/template/default/assets/img/favicons/icon-96x96.png">
+  <link rel="icon" type="image/png" sizes="128x128" href="/html/template/default/assets/img/favicons/icon-128x128.png">
+  <link rel="icon" type="image/png" sizes="144x144" href="/html/template/default/assets/img/favicons/icon-144x144.png">
+  <link rel="icon" type="image/png" sizes="152x152" href="/html/template/default/assets/img/favicons/icon-152x152.png">
+  <link rel="icon" type="image/png" sizes="160x160" href="/html/template/default/assets/img/favicons/icon-160x160.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="/html/template/default/assets/img/favicons/icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="196x196" href="/html/template/default/assets/img/favicons/icon-196x196.png">
+  <link rel="icon" type="image/png" sizes="256x256" href="/html/template/default/assets/img/favicons/icon-256x256.png">
+  <link rel="icon" type="image/png" sizes="384x384" href="/html/template/default/assets/img/favicons/icon-384x384.png">
+  <link rel="icon" type="image/png" sizes="512x512" href="/html/template/default/assets/img/favicons/icon-512x512.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/html/template/default/assets/img/favicons/icon-16x16.png">
+  <link rel="icon" type="image/png" sizes="24x24" href="/html/template/default/assets/img/favicons/icon-24x24.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/html/template/default/assets/img/favicons/icon-32x32.png">
+  <link rel="manifest" href="/html/template/default/assets/img/favicons/manifest.json">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css">
+  <link rel="stylesheet" href="/html/template/default/assets/css/style.css">
+  <link rel="stylesheet" href="/html/template/default/assets/css/ggm-styles.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" defer></script>
+  <link rel="stylesheet" href="/html/user_data/assets/css/customize.css">
 </head>
-<body>
-<div class="ec-layoutRole">
-    <div class="ec-404Role">
-        <div class="ec-off4Grid">
-            <div class="ec-off4Grid__cell">
-                <div style="font-size:100px;text-align:center;">
-                    <div class="ec-404Role__icon ec-icon">
-                        <img src="<?php echo $baseUrl; ?>/html/template/<?php echo $templateCode; ?>/assets/icon/exclamation-pale.svg" alt="">
-                    </div>
-                </div>
-                <p class="ec-404Role__title ec-reportHeading">ただいまメンテナンス中です。</p>
-                <p class="ec-404Role__description ec-reportDescription">大変お手数ですが、しばらくしてから再度アクセスをお願いします。</p>
-            </div>
-        </div>
+<body class="ggm" id="ggm-maintenance">
+<nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand">GGM</span>
+</nav>
+
+<div class="ggm-wrapper">
+  <div class="message-block">
+    <h1 class="text-center">ただいまメンテナンス中です</h1>
+    <div class="maintenance-message">
+    <p class="message-text">大変お手数ですが、しばらくしてから再度アクセスをお願いします。
+    <p class="message-datetime text-center">
+      メンテナンス期間:<br><br>
+      2020年09月25日 (金)<br>
+      AM 0:00 〜 AM 5:00
+    </p>
     </div>
+  </div>
+
+  <footer>
+    <div class="copyright">
+      <p class="copy-right text-center">copyright (c) GGM all rights reserved.</p>
+    </div>
+  </footer>
 </div>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script>
+var eccube_lang = {
+  "common.delete_confirm":"削除してもよろしいですか?"
+}
+</script> 
+<script src="/html/template/default/assets/js/function.js"></script>
+<script src="/html/template/default/assets/js/eccube.js"></script>
+<script src="/html/user_data/assets/js/customize.js"></script>
+<script src="/html/template/default/assets/js/ggm.js"></script>
 </body>
 </html>
