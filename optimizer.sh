c6d55fc7447fc256b3fca5bb8b79bd160d9e2c6b
aonuma-mori <55645909+aonuma-mori@users.noreply.github.com>
2020-10-07 20:47:34 +0900

    Develop a5 (#10)
    * auto commit: 20201002_021039
    
    * auto commit: 20201002_021614
    
    * auto commit: 20201002_023358
    
    * mypage index
    
    * 会員登録完了画面雛形
    
    * mypage top2
    
    * 開発環境が壊れたとき用にディレクトリ作成追加
    
    * auto commit: 20201006_100722
    
    * auto commit: 20201006_100731
    
    * auto commit: 20201006_100820
    
    * auto commit: 20201006_101057
    
    * auto commit: 20201006_101205
    
    * auto commit: 20201006_101737
    
    * auto commit: 20201006_101855
    
    * auto commit: 20201006_102602
    
    * auto commit: 20201006_124007
    
    * 開発環境構築のために修正
    
    * auto commit: 20201006_125752
    
    * auto commit: 20201006_191319
    
    Co-authored-by: Yamakami-O <osamuyamakami@osamunoMacBook-Pro.local>

--- optimizer.sh
+++ optimizer.sh
@@ -20,6 +20,22 @@ done < ./.env
 git add -A;
 git commit -m "auto commit: "`date "+%Y%m%d_%H%M%S"`;
 
+# All backup
+if [ ! -e ./var/tmp/backup ]; then
+mkdir -m 777 ./var/tmp/backup
+fi
+# - source rsync
+# /Users/osamuyamakami/Documents/mywork/docker/ggm
+echo "source copy....";
+rsync -av --delete --exclude 'var/tmp/backup' --exclude '.git' /Users/osamuyamakami/Documents/mywork/docker/ggm ./var/tmp/backup/ggm_`date "+%Y%m%d"`
+tar -pcvzf ./var/tmp/backup/ggm_`date "+%Y%m%d"`.tar.gz ./var/tmp/backup/ggm_`date "+%Y%m%d"`
+rm -fR ./var/tmp/backup/ggm_`date "+%Y%m%d"`
+echo "source copyed !";
+# - mysqldump
+docker exec -it ggm_mysql_1 mysqldump -u dbuser -psecret eccubedb > var/tmp/backup/eccubedb_dump_`date "+%Y%m%d"`.sql
+echo "dump all database /var/tmp/backup"
+
+
 # composer update
 if [ $ENV = "prod" ];  then
 /usr/local/php7.1/bin/php composer.phar update
@@ -34,7 +50,7 @@ rm -fR var/cache/prod/*
 # dev env only
 if [ $ENV = "dev" ];  then
 # db dump
-docker exec -it ec-cube_mysql_1 mysqldump -u dbuser -psecret eccubedb > var/tmp/dump_`date "+%Y%m%d_%H%M%S"`.sql
+docker exec -it ggm_mysql_1 mysqldump -u dbuser -psecret eccubedb > var/tmp/dump_`date "+%Y%m%d_%H%M%S"`.sql
 # docker
 docker-compose up -d
 docker-compose ps -a
--- starter.sh
+++ starter.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
+# make dir
+# var/cache
+if [ ! -e ./var/cache ]; then
+mkdir -m 777 ./var/cache
+fi
+
+# var/log
+if [ ! -e ./var/log ]; then
+mkdir -m 777 ./var/log
+fi
+
+# var/sessions
+if [ ! -e ./var/sessions ]; then
+mkdir -m 777 ./var/sessions
+fi
+
+# var/tmp
+if [ ! -e ./var/tmp ]; then
+mkdir -m 777 ./var/tmp
+fi
+
+
+# docker
 docker-compose up -d
 docker-compose ps -a
 sleep 5
-docker exec ec-cube_mysql_1 mysqldump -u dbuser -psecret eccubedb < var/tmp/develop_dump.sql
+
+# initiarize
+#docker-compose exec ec-cube composer run-script compile
+
+# restore
+docker exec ggm_mysql_1 mysqldump -u dbuser -psecret eccubedb < var/tmp/develop_dump.sql
+
+
+
