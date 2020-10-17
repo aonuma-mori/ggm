14cffdf0fbdd07d4ef0159aaa6757addc79b2b90
aonuma-mori <55645909+aonuma-mori@users.noreply.github.com>
2020-10-17 15:54:40 +0900

    Develop a5 (#12)
    * auto commit: 20201002_021039
    
    * auto commit: 20201002_021614
    
    * auto commit: 20201002_023358
    
    * mypage index
    
    * 会員登録完了画面雛形
    
    * mypage top2
    
    * 開発環境が壊れたとき用にディレクトリ作成追加
    
    * auto commit: 20201006_100722
    
    * auto commit: 20201006_100731
    
    * auto commit: 20201006_100820
    
    * auto commit: 20201006_101057
    
    * auto commit: 20201006_101205
    
    * auto commit: 20201006_101737
    
    * auto commit: 20201006_101855
    
    * auto commit: 20201006_102602
    
    * auto commit: 20201006_124007
    
    * 開発環境構築のために修正
    
    * auto commit: 20201006_125752
    
    * auto commit: 20201006_191319
    
    * auto commit: 20201007_145140
    
    * auto commit: 20201007_195628
    
    * RSSからTwitterを読み込む
    
    * auto commit: 20201008_182957
    
    * wordpressのfeedを読み込む
    
    * wordpressのrssをtwigに組み込み
    
    * トップページにRSSフィード設置
    
    * cssをミニマイズしたけど、元に戻した
    
    * 商品画像
    
    * 商品登録とトップページ
    
    Co-authored-by: Yamakami-O <osamuyamakami@osamunoMacBook-Pro.local>

--- src/Eccube/Controller/TopController.php
+++ src/Eccube/Controller/TopController.php
@@ -25,10 +25,96 @@ class TopController extends AbstractController
   public function index()
   {
     /**
-     * RSSを読み込む?(やめた。面倒なので。)
-     * 
+     * 商品リンクの出し分け　トップ3
      */
-    // var_dump("foobar");
-    return [];
+    if (env("APP_ENV") == "prod") {
+      $product_links = [
+        "/products/list?category_id=10",
+        "/products/list?category_id=7",
+        "/products/list?category_id=11",
+      ];
+    } else {
+      $product_links = [
+        "/products/list?category_id=10",
+        "/products/list?category_id=7",
+        "/products/list?category_id=11",
+      ];
+    }
+
+    /**
+     * TwitterのRSSを読み込む
+     * 
+     * twitter RSS service: http://twitter-great-rss.herokuapp.com/
+     * url: http://twitter-great-rss.herokuapp.com/feed/user?name=GGM06605451&url_id_hash=4aabb9cbdf72d63df551b752ac8f63181466b24c
+     */
+    $rss_url = "http://twitter-great-rss.herokuapp.com/feed/user?name=GGM06605451&url_id_hash=4aabb9cbdf72d63df551b752ac8f63181466b24c";
+    $rss = file_get_contents($rss_url);
+    $rss = simplexml_load_string($rss);
+    foreach ($rss->channel as $v) {
+      $tweet = $v->item->description;
+    }
+    $dummy = preg_match('/https?:\/{2}[\w\/:%#\$&\?\(\)~\.=\+\-]+/', $tweet, $matches);
+    $tweet = preg_replace("/<img(.|\s)*?>/","",$tweet);
+    $image = $matches[0];
+
+    /**
+     * Wordpress RSS
+     * 
+     * url: https://ggm-do.com/blog/feed/
+     */
+    if (env("APP_ENV") == "dev") {
+      $wp_rss_url = "https://ggm-do.com/blog/feed/";
+      // $wp_rss_url = "http://127.0.0.1:9100/blog/feed/";
+    } else {
+      $wp_rss_url = "https://ggm-do.com/blog/feed/";
+    }
+
+    if ($this->httpstatus($wp_rss_url) == "200") {
+      $wp_rss = file_get_contents($wp_rss_url);
+      $wp_rss = simplexml_load_string($wp_rss);
+      // $wp_blog_array = json_decode(json_encode($wp_rss->channel->item), true);
+      $wp_rss_object = $wp_rss->channel->item;
+    } else {
+      $wp_rss_object = array();
+    }
+    // dump($wp_rss->channel->item);
+
+
+
+    log_debug('GGMログメッセージ');
+    // log_alert('GGMログメッセージ');
+    // log_critical('GGMログメッセージ');
+    // log_error('GGMログメッセージ');
+    // log_warning('GGMログメッセージ');
+    // log_notice('GGMログメッセージ');
+    // log_info('GGMログメッセージ');
+    // log_debug('GGMログメッセージ');
+
+    return [
+      'wp' => $wp_rss_object,
+      'tweet' => $tweet,
+      'image_url' => $image,
+      'product_links' => $product_links,
+    ]; 
+  }
+
+  
+  /**
+   * RSSなどのHTTPステータスを確認する
+   * 
+   * 200: true
+   */
+  public function httpstatus($url) {
+
+    $curl = "curl -LI ".$url." -o /dev/null -w '%{http_code}\n' -s";
+    $httpstatus = exec($curl, $error);
+    // logging
+    // log_info('GGMログメッセージ');
+
+    // var_dump($httpstatus);
+    return $httpstatus;
   }
 }
+
+
+
