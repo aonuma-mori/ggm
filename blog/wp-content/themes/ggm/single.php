2bc5c7e94e1030714ee5dfb7c0db5bf6c3650e4e
aonuma <aonuma.mori@gmail.com>
2020-10-26 09:18:07 +0900

    いろいろ

--- blog/wp-content/themes/ggm/single.php
+++ blog/wp-content/themes/ggm/single.php
@@ -27,7 +27,7 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="blog-item">
           <h1 class="blog-title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <?php the_title(); ?>
           </h1>
           <div class="blog-text">
             <?php the_content(); ?>
@@ -54,13 +54,13 @@
         <!--sidebar-->
         <?php get_template_part('inc/admintools'); ?>
 
-        <ul>
+        <!-- <ul>
           <li><a href="">foobar</a></li>
           <li><a href="">foobar</a></li>
           <li><a href="">foobar</a></li>
           <li><a href="">foobar</a></li>
           <li><a href="">foobar</a></li>
-        </ul>
+        </ul> -->
       </div><!-- col sub -->
 
     </div><!-- row -->
@@ -68,40 +68,7 @@
 
 
 
-<div class="ec-layoutRole__mainBottom">
-  <div class="ggm-ec-role ec-role">
-    <div class="container eyecatch-block">
-      <div class="row">
-        <div class="col-sm-4 eyecatch-block eyecatch-block-01">
-          <a href="#">
-            <img src="/html/template/default/assets/img/top/eyecatch-01.jpg" class="eyecatch-img eyecatch-img-01">
-          </a>
-          <div class="text-center eyecatch-title eyecatch-title01">鞣し生成り麻縄</div>
-          <div class="eyecatch-summary">麻縄本来の色と艶、強度を備えています。手入れの仕方次第で何年も品質を保持できます。<br>
-            <a href="#">商品を見る</a>
-          </div>
-        </div>
-        <div class="col-sm-4 eyecatch-block eyecatch-block-02">
-          <a href="#">
-            <img src="/html/template/default/assets/img/top/eyecatch-02.jpg" class="eyecatch-img eyecatch-img-02">
-          </a>
-          <div class="text-center eyecatch-title eyecatch-title02">素麻縄</div>
-          <div class="eyecatch-summary">生の麻縄そのままの状態です。お好みの油で鞣してお使いください。<br>
-            <a href="#">商品を見る</a></div>
-          </div>
-          <div class="col-sm-4 eyecatch-block eyecatch-block-03">
-            <a href="#">
-              <img src="/html/template/default/assets/img/top/eyecatch-03.jpg" class="eyecatch-img eyecatch-img-03">
-            </a>
-            <div class="text-center eyecatch-title eyecatch-title03">染麻縄</div>
-            <div class="eyecatch-summary">色がついていてかっこいい。カラーSAMPLEから選べます。オリジナル色もできます。<br>
-              <a href="#">商品を見る</a></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
+  <?php get_template_part('inc/products'); ?> 
   <?php get_template_part('inc/footer'); ?>   
   </div><!-- ggm-wrapper -->
 
